08122c2695cf10eeedd093f5ad8c2de4b5ff8626
Dea <dea.chaudhery@gmail.com>
2020-10-05 14:36:12 -0400

    calculate cost of using AWS

---
+++ AWS.py
@@ -0,0 +1,8 @@
+import pymongo
+
+connection = pymongo.MongoClient('localhost', 27017)
+database = connection["mydb_01"]
+collection = database["mycol_01"]
+
+data = {'Name': 'Dea'}
+collection.insert_one(data)
